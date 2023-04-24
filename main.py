6bcb2ce25da96e55726db1b1163c7815df9b644e
Ruslan <113181317+R-Ohman@users.noreply.github.com>
2023-04-24 16:19:34 +0200

    Update main.py
    Call the tests in ascending file number order.
    Continuation of the program if the time limit is over.

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import subprocess
 import difflib
 import time
 
-PROGRAM_PATH = "C:\\Users\\Ptryb\\source\\repos\\AiSD1\\x64\\Debug\\AiSD1.exe"
+PROGRAM_PATH = "E:\\Folder\\Program.exe"
 
+
+def test_key(filename):
+    return int(os.path.splitext(filename)[0])
 
 def search_for_tests():
     for root, dirs, files in os.walk('tests'):
-        for file in files:
+        for file in sorted(files, key=test_key):
             if file.endswith('.in'):
                 yield os.path.join(root, file).rstrip(".in")
 
@@ -17,7 +20,11 @@ def run_test(input_file, output_file):
     with open(input_file, 'r') as in_file:
         with open(output_file, 'w') as out_file:
             proc = subprocess.Popen(PROGRAM_PATH, stdin=in_file, stdout=out_file)
-            return_code = proc.wait(3)
+            try:
+                return_code = proc.wait(4)
+            except subprocess.TimeoutExpired:
+                proc.kill()
+                return_code = -1
     return return_code
 
 
@@ -42,7 +49,7 @@ if __name__ == '__main__':
                 failed += 1
                 print(diff)
             else:
-                print('Test passed! Time: {:.2f} s'.format(time.time() - start_time))
+                print('Test passed! Time: {:.4} s'.format(time.time() - start_time))
                 passed += 1
         else:
             print('Runtime error!')
